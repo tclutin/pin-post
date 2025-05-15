2dcc6d3d46ebbdbc710ff2949a6862a398928931
Lutin <tclutin01@gmail.com>
2025-05-15 21:00:55 +0500

    feat: app.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/test', function () {
-    return "test";
 });
