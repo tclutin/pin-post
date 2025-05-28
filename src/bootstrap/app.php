9d09b2ad6a0fcbcf6ee6b660c30b79790ec85f9a
Elizaveta Pernikova <lisapernikowa@mail.ru>
2025-05-28 21:21:49 +0500

    ffeat: cors

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(ForceJsonResponse::class);
+        $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
